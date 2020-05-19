3c5baa451ac2f84139bfc761f9d561cf917871a1
BakhtiarNurzaman <talokcity45@gmail.com>
2020-05-19 08:39:09 +0700

    update nama

--- application/views/welcome/welcome_message.php
+++ application/views/welcome/welcome_message.php
@@ -20,7 +20,7 @@
 			<!-- Default box -->
 			<div class="box">
 				<div class="box-header with-border">
-					<h3 class="box-title">Dashboard</h3>
+					<h3 class="box-title">Dashboard C</h3>
 
 					<div class="box-tools pull-right">
 						<button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
